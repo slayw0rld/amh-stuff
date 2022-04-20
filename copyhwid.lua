164dbc606710c695164f9c3cdf94c08a51ac55f5
slayw0rld <104044859+slayw0rld@users.noreply.github.com>
2022-04-19 22:15:42 -0400

    Update copyhwid.lua

--- copyhwid.lua
+++ copyhwid.lua
@@ -1,1 +1,10 @@
+local NotifyLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/farthefarter/akanotif/main/lua"))()
+local Notify = NotifyLibrary.Notify
+
 setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
+Notify({
+                    Title = "amh hwid copier",
+                    Description = "copied hwid!",
+                    Duration = 5
+                })
+            end
